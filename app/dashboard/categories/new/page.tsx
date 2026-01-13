4ad4bfb7e59760e9f52b8aaf631f5dba5509ad88
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2026-01-13 17:58:27 +0000

    Fix component name and import

--- app/dashboard/categories/new/page.tsx
+++ app/dashboard/categories/new/page.tsx
@@ -1,4 +1,6 @@
-export default function CreateCategoryForm() {
+import CreateCategoryForm from "./create-category-form";
+
+export default function CreateCategoryPage() {
   return (
     <section>
       <h1 className="text-2xl font-bold">Create new category</h1>
